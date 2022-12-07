cdd0f36bf41bb5ac7c4c6a70c2076933e4662a98
Mikiawm <Mikiawm@gmail.com>
2022-12-07 14:50:12 +0100

    day7 - 2022

--- 2022/day6/day6.js
+++ 2022/day6/day6.js
@@ -2,22 +2,18 @@ const fs = require('fs');
 
 var input = fs.readFileSync('./input.txt', 'utf-8');
 
-// Part 1
-// var len = 4;
+console.log("Part1: " + findSize(4));
+console.log("Part2: " + findSize(14));
 
-// Part 2
-var len = 14;
+function findSize(len) {
+    for (let index = 0; index < input.length - len; index++) {
 
-for (let index = 0; index < input.length - len; index++) {
+        var part = input.substring(index, index + len).split('');
+        var st = new Set(part);
 
-    var part = input.substring(index, index + len).split('');
+        if (st.size === len) {
 
-    var st = new Set(part);
-
-
-    if (st.size === len) {
-        console.log(index + len); 
-        return 0;
+            return index + len;
+        }
     }
-    
 }
