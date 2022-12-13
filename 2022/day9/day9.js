7ed9a5924eb403c5f3c7b2522beca00ab5125c73
Mikiawm <Mikiawm@gmail.com>
2022-12-13 09:11:46 +0100

    day 9 Part2 2022

--- 2022/day9/day9.js
+++ 2022/day9/day9.js
@@ -4,59 +4,98 @@ var input = fs.readFileSync('./input.txt', 'utf-8');
 
 var data = input.split("\r\n");
 
-var hPos = { x: 0, y: 0 };
-var tPos = { x: 0, y: 0 };
-var tPos = { x: 0, y: 0 };
+var rope = [];
 
-var tPosLst = [JSON.stringify(tPos)];
+// Solve part1
+var part1 = 2;
+
+// Solve part2
+var part2 = 10;
+
+for (let i = 0; i < part2; i++) {
+    rope.push([0, 0]);
+}
+
+var tPosLst = [JSON.stringify(rope[0])];
 
 const isAdjacent = (h, t) => {
     const isAdjacent = [
-        { x: t.x, y: t.y }, // Same 
-        { x: t.x, y: t.y + 1 }, // Top
-        { x: t.x, y: t.y - 1 }, // Bottom
-        { x: t.x - 1, y: t.y }, // Left
-        { x: t.x + 1, y: t.y }, // Right
-        { x: t.x - 1, y: t.y - 1 }, // Top left
-        { x: t.x + 1, y: t.y - 1 }, // Bottom left
-        { x: t.x - 1, y: t.y + 1 }, // Top right
-        { x: t.x + 1, y: t.y + 1 } // Bottom right
-    ].some(element => element.x == h.x && element.y == h.y)
+        { x: t[0], y: t[1] }, // Same 
+        { x: t[0], y: t[1] + 1 }, // Top
+        { x: t[0], y: t[1] - 1 }, // Bottom
+        { x: t[0] - 1, y: t[1] }, // Left
+        { x: t[0] + 1, y: t[1] }, // Right
+        { x: t[0] - 1, y: t[1] - 1 }, // Top left
+        { x: t[0] + 1, y: t[1] - 1 }, // Bottom left
+        { x: t[0] - 1, y: t[1] + 1 }, // Top right
+        { x: t[0] + 1, y: t[1] + 1 } // Bottom right
+    ].some(element => element.x == h[0] && element.y == h[1])
 
     return isAdjacent;
-};
+}
+
 
 data.forEach(element => {
     var [d, v] = element.split(" ");
     for (let index = 0; index < v; index++) {
-        if (d == "R") {
-            hPos.x += 1;
-            if (!isAdjacent(hPos, tPos)) {
-                tPos.x = hPos.x - 1;
-                tPos.y = hPos.y;
+        rope.forEach((part, ropeIndex) => {
+            if (ropeIndex == 0) {
+                rope[0] = moveHead(part);
             }
-        } else if (d == "L") {
-            hPos.x -= 1;
-            if (!isAdjacent(hPos, tPos)) {
-                tPos.x = hPos.x + 1;
-                tPos.y = hPos.y;
+            else if (ropeIndex == rope.length - 1) {
+                var head = rope[ropeIndex - 1];
+                var cur = rope[ropeIndex];
+                rope[ropeIndex] = moveTail(head, cur);
+                tPosLst.push(JSON.stringify(rope[ropeIndex]))
             }
-        } else if (d == "U") {
-            hPos.y += 1;
-            if (!isAdjacent(hPos, tPos)) {
-                tPos.y = hPos.y - 1;
-                tPos.x = hPos.x;
+            else {
+                var head = rope[ropeIndex - 1];
+                var cur = rope[ropeIndex];
+                rope[ropeIndex] = moveTail(head, cur);
             }
-        } else if (d == "D") {
-            hPos.y -= 1;
-            if (!isAdjacent(hPos, tPos)) {
-                tPos.y = hPos.y + 1;
-                tPos.x = hPos.x;
-            }
-        }
-        tPosLst.push(JSON.stringify(tPos));
+        });
     }
 
+
+    function moveHead(hPos) {
+        if (d == "R") {
+            return [hPos[0] += 1, hPos[1]];
+        } else if (d == "L") {
+            return [hPos[0] -= 1, hPos[1]]
+        } else if (d == "U") {
+            return [hPos[0], hPos[1] += 1]
+        } else if (d == "D") {
+            return [hPos[0], hPos[1] -= 1]
+        }
+    }
+
+    function moveTail(h, t) {
+        if (!isAdjacent(h, t)) {
+            var xDif = h[0] - t[0];
+            if (xDif >= 1) {
+                xDif = 1;
+            }
+            else if (xDif == 0) {
+                xDif = 0;
+            }
+            else {
+                xDif = -1;
+            }
+            var yDif = h[1] - t[1];
+            if (yDif >= 1) {
+                yDif = 1;
+            }
+            else if (yDif == 0) {
+                yDif = 0;
+            }
+            else {
+                yDif = -1;
+            }
+
+            return [t[0] + xDif, t[1] + yDif]
+        }
+        return t;
+    }
 });
 
 console.log(new Set(tPosLst).size);
